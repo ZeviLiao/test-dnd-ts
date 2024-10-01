b42dd19c7c46852facd5ddcbae3f54de903b345d
Zevi Liao <zevi.liao@gosugamers.net>
2024-10-01 18:24:26 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react";
+import React, { useState } from "react";
 import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
